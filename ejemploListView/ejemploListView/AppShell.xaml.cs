b75eeecedb6081c67d17e061a4913a3ce2f55768
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-08-27 13:21:17 -0600

    listView

---
+++ ejemploListView/ejemploListView/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ejemploListView
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ejemploListView/ejemploListView/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace ejemploListView
+{
+    public partial class MainPage : ContentPage
+    {   
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            /*
+            //Data lista de strings
+            List<string> contacts = new List<string>()
+            {
+                "Hanzeel",
+                "Santi",
+                "Fernando",
+            };
+            */
+
+            //Data lista de Contact
+            List<Contact> contacts = new List<Contact>
+            {
+                new() {Name="Hanzeel", Email="email.com"},
+                new() {Name="Santi", Email="email.com.mx"},
+                new() {Name="Angel", Email="email.com.mx"},
+
+            };
+
+            //Decirle a listView que data mostrar
+            listContacts.ItemsSource = contacts;
+        }
+    }
+    
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+} 
